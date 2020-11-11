a625f0e5cb9af125c891ca5b12ba56aac7c70d1c
DanielALarrea <daniellarrea2697@gmail.com>
2020-11-11 15:19:21 -0600

    Added Cart class
    Cart class might serve as base for Invoice in database

---
+++ src/com/ecommerce/beans/Cart.java
@@ -0,0 +1,59 @@
+package com.ecommerce.beans;
+
+import java.util.List;
+
+public class Cart {
+	
+	private User customer;
+	private List<Product> cartItems;
+	private float totalPrice;
+		
+	public Cart() {
+		
+	}
+
+	public Cart(User customer, List<Product> cartItems) {
+		super();
+		this.customer = customer;
+		this.cartItems = cartItems;
+		calculateTotalPrice();
+	}
+
+	public User getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(User customer) {
+		this.customer = customer;
+	}
+
+	public List<Product> getCartItems() {
+		return cartItems;
+	}
+
+	public void setCartItems(List<Product> cartItems) {
+		this.cartItems = cartItems;
+	}
+	
+	public void addToCart(Product product) {
+		this.cartItems.add(product);
+		calculateTotalPrice();
+	}
+	
+	public void removeFromCart(Product product) {
+		this.cartItems.remove(product);
+		calculateTotalPrice();
+	}
+	
+	public void calculateTotalPrice() {
+		float totalPrice = 0;
+		for(Product product: this.cartItems) {
+			totalPrice += product.getProductPrice();
+		}
+		this.totalPrice = totalPrice;
+	}
+	
+	public float getTotalPrice() {
+		return this.totalPrice;
+	}
+}
